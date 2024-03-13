3b11fbbf2397fb98a546aebbe517f0bdd11f380b
detroitnatif <tytydizzle124@gmail.com>
2024-03-13 09:44:10 +0100

    Adding grok API call

--- LangchainSearch.py
+++ LangchainSearch.py
@@ -1,11 +1,14 @@
 import os
 from dotenv import load_dotenv
 from langchain.llms import OpenAI
-from langchain.agents import load_tools
+from langchain.agents import load_tools, initialize_agent, AgentType
+
 
 load_dotenv()
 
 llm = OpenAI(temperature=0.5, streaming=True, openai_api_key=os.getenv('API_KEY'))
-
+tools = load_tools(
+    ''
+)
 
 
